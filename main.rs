8124d50883b1a0c59954a0492317aba572a5847a
umaresso <umarkhatab467@gmail.com>
2022-07-03 23:13:22 -0700

    Obtained filenames of current directory

--- main.rs
+++ main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut paths = fs::read_dir("./").unwrap();
+    let mut filenames:Vec<String>=Vec::new();
+    for path in &mut paths {
+        let mut path_=path.iter();
+        if let Some(a)=path_.next(){
+            let fname = a.file_name();
+            let name_=fname.to_str().clone();
+             let mut name=String::new();
+            if let Some(string)=name_{
+                name = String::from(string).clone();
+            }
+            filenames.push(name);
+
+        }
+
+    }
+
+    println!("Filenames are ");
+    for elem in filenames{
+        println!("{}",elem);
+    }
+
+
 }
